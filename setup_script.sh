0dd427eced1729049d89e7dbe4f8295d9e978c43
Giffoni Lopes <kgiffoni_@tuta.com>
2025-12-27 09:34:24 -0300

    removed shell setup

--- setup_script.sh
+++ setup_script.sh
@@ -4,7 +4,7 @@ install_apps() {
 
   sudo pacman -Syu
 
-  sudo pacman -S --needed fish firefox micro niri wayland xorg xwayland-satellite code foot nicotine+ fuzzel vesktop-bin jdk-openjdk waydroid otf-monaspace swaybg steam timidity++ mpd ncmpcpp mako brightnessctl python python-pipx flatpak tree celluloid loupe darkman fragments errands
+  sudo pacman -S --needed firefox micro niri wayland xorg xwayland-satellite code foot nicotine+ fuzzel vesktop-bin jdk-openjdk waydroid otf-monaspace swaybg steam timidity++ mpd ncmpcpp mako brightnessctl python python-pipx flatpak tree celluloid loupe fragments errands
 
   pipx install "beets[fetchart,embedart,lastgenre,scrub,replaygain,thumbnails,info]"
   pipx ensurepath
@@ -21,15 +21,12 @@ install_apps() {
 
 configure_apps() {
 
-  chsh -s /usr/bin/fish
-
   mkdir -p $HOME/.local/state/mpd/
   touch $HOME/.local/state/mpd/log
 
   install -D ./giffoni/Pictures/Wallpaper/wallpaper.jpg $HOME/Pictures/Wallpaper/wallpaper.jpg
   install -D ./giffoni/.ncmpcpp/config $HOME/.ncmpcpp/config
   install -D ./giffoni/.config/beets $HOME/.config/beets
-  install -D ./giffoni/.config/darkman/config.yaml $HOME/.config/darkman/config.yaml
   install -D ./giffoni/.config/foot/foot.ini $HOME/.config/foot/foot.ini
   install -D ./giffoni/.config/fuzzel/fuzzel.ini $HOME/.config/fuzzel/fuzzel.ini
   install -D ./giffoni/.config/mako/config $HOME/.config/mako/config
@@ -40,7 +37,6 @@ configure_apps() {
     systemctl --user enable --now mako
     systemctl --user enable --now mpd
     systemctl --user enable --now mpd-discord-rpc
-    systemctl --user enable --now darkman.service
     
     git config --global color.ui auto
     git config --global user.name "Giffoni Lopes"
