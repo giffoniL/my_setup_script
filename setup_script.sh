b3d9bc41c6993e2131508ab3091579ec53406533
Giffoni Lopes <kgiffoni_@tuta.com>
2025-12-26 23:54:43 -0300

    new packages new configs yadayada

--- setup_script.sh
+++ setup_script.sh
@@ -4,20 +4,16 @@ install_apps() {
 
   sudo pacman -Syu
 
-  sudo pacman -S --needed firefox micro niri wayland xorg xwayland-satellite code ghostty nicotine+ rofi vesktop-bin jdk-openjdk waydroid otf-monaspace swaybg swayimg steam timidity++ mpd ncmpcpp mako brightnessctl python python-pipx obsidian flatpak tree celluloid inter-font qbittorrent
+  sudo pacman -S --needed fish firefox micro niri wayland xorg xwayland-satellite code foot nicotine+ fuzzel vesktop-bin jdk-openjdk waydroid otf-monaspace swaybg steam timidity++ mpd ncmpcpp mako brightnessctl python python-pipx flatpak tree celluloid loupe darkman fragments errands
 
   pipx install "beets[fetchart,embedart,lastgenre,scrub,replaygain,thumbnails,info]"
   pipx ensurepath
 
-  paru -S mpd-discord-rpc todoist-appimage apple_cursor
+  paru -S mpd-discord-rpc apple_cursor
 
-  git clone https://github.com/vinceliuice/Fluent-gtk-theme.git
+  git clone https://github.com/yeyushengfan258/Reversal-icon-theme.git
 
-  ./Fluent-gtk-theme/install.sh -l -s compact --tweaks round -c dark
-
-  git clone https://github.com/vinceliuice/Fluent-icon-theme.git
-
-  ./Fluent-icon-theme/install.sh
+  ./Reversal-icon-theme/install.sh -t blue
 
   echo -e "\033[32mFinished installing apps.\033[0m"
 
@@ -25,17 +21,26 @@ install_apps() {
 
 configure_apps() {
 
-  chsh -s /usr/bin/bash
+  chsh -s /usr/bin/fish
+
+  mkdir -p $HOME/.local/state/mpd/
+  touch $HOME/.local/state/mpd/log
 
   install -D ./giffoni/Pictures/Wallpaper/wallpaper.jpg $HOME/Pictures/Wallpaper/wallpaper.jpg
   install -D ./giffoni/.ncmpcpp/config $HOME/.ncmpcpp/config
-  install -D ./giffoni/.config/beets/config.yaml $HOME/.config/beets/config.yaml
-  install -D ./giffoni/.config/ghostty/config $HOME/.config/ghostty/config
+  install -D ./giffoni/.config/beets $HOME/.config/beets
+  install -D ./giffoni/.config/darkman/config.yaml $HOME/.config/darkman/config.yaml
+  install -D ./giffoni/.config/foot/foot.ini $HOME/.config/foot/foot.ini
+  install -D ./giffoni/.config/fuzzel/fuzzel.ini $HOME/.config/fuzzel/fuzzel.ini
+  install -D ./giffoni/.config/mako/config $HOME/.config/mako/config
+  install -D ./giffoni/.config/mimeapps.list $HOME/.config/mimeapps.list
   install -D ./giffoni/.config/mpd/mpd.conf $HOME/.config/mpd/mpd.conf
   install -D ./giffoni/.config/niri/config.kdl $HOME/.config/niri/config.kdl
 
+    systemctl --user enable --now mako
     systemctl --user enable --now mpd
     systemctl --user enable --now mpd-discord-rpc
+    systemctl --user enable --now darkman.service
     
     git config --global color.ui auto
     git config --global user.name "Giffoni Lopes"
