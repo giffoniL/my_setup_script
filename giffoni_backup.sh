043918e9664e91fe8dd2db884613098256e5fa5f
Giffoni Lopes <kgiffoni_@tuta.com>
2026-06-02 20:35:47 -0300

    personal backup script

---
+++ giffoni_backup.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# this is a personal script for backuping my current set of personal files into my HD
+# stuff like pictures, books, music, etc etc. stuff that i can't just publicly host
+
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+log() {
+    echo -e "${GREEN}[INFO]: $1${NC}"
+}
+
+warn() {
+    echo -e "${YELLOW}[WARNING]: $1${NC}"
+}
+
+error() {
+    echo -e "${RED}[ERROR]: $1${NC}"
+    exit 1
+}
+
+while true; do
+    read -p "Are you Giffoni, and is your external hard drive plugged in? (y/n): " yn
+    case $yn in
+    [Yy]*)
+
+        log "Mounting external hard drive..."
+        sudo mount /dev/sda1 /mnt
+
+        log "Backuping files into external hard drive..."
+        rsync -av --delete --progress $HOME/.mpdscribble /mnt/
+        rsync -av --delete --progress $HOME/Music /mnt/
+        rsync -av --delete --progress $HOME/Code /mnt/
+
+        log "Unmounting external hard drive..."
+        sudo umount /mnt
+
+        break
+        ;;
+    [Nn]*)
+        log "Alrighty then..."
+        break
+        ;;
+    *)
+        warn "Please answer yes or no."
+        ;;
+    esac
+done
--- setup_script.sh
+++ setup_script.sh
@@ -69,7 +69,7 @@ check_deps() {
 install_apps() {
     BASE_PKGS=(git rsync nano fastfetch greetd greetd-agreety fish fisher github-cli micro jdk-openjdk shfmt otf-monaspace ttf-nerd-fonts-symbols noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra timidity++ mpd mpc rmpc mpdscribble cava brightnessctl flatpak tree beets bash-completion chromaprint ffmpeg gst-plugins-bad gst-plugins-good gst-plugins-ugly gst-libav gst-python imagemagick python-beautifulsoup4 python-discogs-client python-flask python-gobject python-langdetect python-librosa python-mpd2 python-pyacoustid python-pylast python-requests-oauthlib python-xdg python-titlecase)
     DESKTOP_PKGS=(wayland niri xorg xwayland-satellite wl-clipboard fuzzel mako foot polkit-gnome xdg-desktop-portal xdg-desktop-portal-gnome gnome-keyring awww swayidle)
-    APP_PKGS=(firefox zed nicotine+ nautilus vesktop gimp steam celluloid loupe fragments obsidian seahorse gaphor solanum)
+    APP_PKGS=(firefox zed nicotine+ nautilus vesktop gimp steam celluloid loupe fragments obsidian seahorse gaphor solanum errands)
 
     PACMAN_PKGS=("${BASE_PKGS[@]}" "${DESKTOP_PKGS[@]}" "${APP_PKGS[@]}")
     PARU_PKGS=(mpd-discord-rpc)
